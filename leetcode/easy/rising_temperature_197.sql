1a31699905f81648b8b7b8c5de8cf1a847c99021
spallavi13 <117496287+spallavi13@users.noreply.github.com>
2023-03-27 23:33:26 -0700

    Create rising_temperature_197.sql

---
+++ leetcode/easy/rising_temperature_197.sql
@@ -0,0 +1,9 @@
+# leetcode problem
+# 197. Rising Temperature
+
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+INNER JOIN Weather w2
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
