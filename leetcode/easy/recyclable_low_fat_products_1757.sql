99d9615eee94de34bba008923ae7cd1593a6085d
spallavi13 <117496287+spallavi13@users.noreply.github.com>
2023-03-27 23:09:08 -0700

    Update SQL problems

---
+++ leetcode/easy/recyclable_low_fat_products_1757.sql
@@ -0,0 +1,8 @@
+# leetcode problem:
+# 1757. Recyclable and Low Fat Products
+
+# Write your MySQL query statement below
+select product_id from Products
+where( low_fats ='Y'
+AND
+recyclable ='Y')
